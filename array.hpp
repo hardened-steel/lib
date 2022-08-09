c3812405499e5bc2f3a75a1643c551a84e4b651c
hardened-steel <keldgaden@gmail.com>
2022-08-09 20:28:04 +0300

    fix array.hpp

--- array.hpp
+++ array.hpp
@@ -2,6 +2,11 @@
 #include <array>
 
 namespace lib {
+    namespace details {
+        template<class T, std::size_t N>
+        using RawArray = T(&)[N];
+    }
+
     template<std::size_t ...Ia, std::size_t ...Ib>
     constexpr auto concat(const std::array<char, sizeof...(Ia)>& a, std::index_sequence<Ia...>, const std::array<char, sizeof...(Ib)>& b, std::index_sequence<Ib...>) noexcept
     {
@@ -15,25 +20,25 @@ namespace lib {
     }
 
     template<std::size_t ...Ia, std::size_t ...Ib>
-    constexpr auto concat(const std::array<char, sizeof...(Ia)>& a, std::index_sequence<Ia...>, const char (&b)[sizeof...(Ib) + 1u], std::index_sequence<Ib...>) noexcept
+    constexpr auto concat(const std::array<char, sizeof...(Ia)>& a, std::index_sequence<Ia...>, details::RawArray<const char, sizeof...(Ib) + 1u> b, std::index_sequence<Ib...>) noexcept
     {
         return std::array<char, sizeof...(Ia) + sizeof...(Ib)>{a[Ia]..., b[Ib]...};
     }
 
     template<std::size_t A, std::size_t B>
-    constexpr auto concat(const std::array<char, A>& a, const char (&b)[B]) noexcept
+    constexpr auto concat(const std::array<char, A>& a, details::RawArray<const char, B> b) noexcept
     {
         return concat(a, std::make_index_sequence<A>{}, b, std::make_index_sequence<B - 1u>{});
     }
 
     template<std::size_t ...Ia, std::size_t ...Ib>
-    constexpr auto concat(const char (&a)[sizeof...(Ia) + 1u], std::index_sequence<Ia...>, const std::array<char, sizeof...(Ib)>& b, std::index_sequence<Ib...>) noexcept
+    constexpr auto concat(details::RawArray<const char, sizeof...(Ia) + 1u> a, std::index_sequence<Ia...>, const std::array<char, sizeof...(Ib)>& b, std::index_sequence<Ib...>) noexcept
     {
         return std::array<char, sizeof...(Ia) + sizeof...(Ib)>{a[Ia]..., b[Ib]...};
     }
 
     template<std::size_t A, std::size_t B>
-    constexpr auto concat(const char (&a)[A], const std::array<char, B>& b) noexcept
+    constexpr auto concat(details::RawArray<const char, A> a, const std::array<char, B>& b) noexcept
     {
         return concat(a, std::make_index_sequence<A - 1u>{}, b, std::make_index_sequence<B>{});
     }
@@ -45,7 +50,7 @@ namespace lib {
     }
 
     template<std::size_t A, class Arg, class ...Ts>
-    constexpr auto separated(const char (&separator)[A], const Arg& arg, const Ts& ...args) noexcept
+    constexpr auto separated(details::RawArray<const char, A> separator, const Arg& arg, const Ts& ...args) noexcept
     {
         if constexpr(sizeof...(Ts)) {
             return concat(arg, separator, separated(separator, args...));
